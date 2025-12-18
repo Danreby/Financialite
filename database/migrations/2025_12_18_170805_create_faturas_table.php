eec4c9b295879f0fa3d744ec02e3bfe76f6ae898
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-12-18 14:31:50 -0300

    Migrations

---
+++ database/migrations/2025_12_18_170805_create_faturas_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('faturas', function (Blueprint $table) {
+            $table->id();
+            $table->string('title');
+            $table->string('description')->nullable();
+            $table->decimal('amount', 10, 2);
+            $table->date('due_date');
+            $table->enum('type', ['credit', 'debit']);
+            $table->enum('status', ['paid', 'unpaid', 'overdue'])->default('unpaid');
+            $table->date('paid_date')->nullable();
+            $table->integer('total_installments')->default(1);
+            $table->integer('current_installment')->default(1);
+            $table->boolean('is_recurring')->default(false);
+            $table->unsignedBigInteger('user_id')->nullable();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('faturas');
+    }
+};
