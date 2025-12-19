62d406c1f1c2d0099ff0878ecc7ef3041b706271
Bernardo Rolim <rolimbernardo6@gmail.com>
2025-12-19 09:33:01 -0300

    Filtro de faturas

--- app/Models/Fatura.php
+++ app/Models/Fatura.php
@@ -48,4 +48,60 @@ class Fatura extends Model
     {
         return $this->bankUser?->bank;
     }
+
+    public static function filter(
+        $title = null, 
+        $description = null, 
+        $amount = null, 
+        $due_date = null, 
+        $type = null, 
+        $status = null,
+        $paid_date = null, 
+        $is_recurring = null, 
+        $user_id = null, 
+        $bank_user_id = null, 
+        $total_installments = null, 
+        $current_installment = null, 
+    )
+    {
+        $query = self::query();
+
+        if ($title !== null) {
+            $query->where('title', 'like', '%' . $title . '%');
+        }
+        if ($description !== null) {
+            $query->where('description', 'like', '%' . $description . '%');
+        }
+        if ($amount !== null) {
+            $query->where('amount', $amount);
+        }
+        if ($due_date !== null) {
+            $query->where('due_date', $due_date);
+        }
+        if ($type !== null) {
+            $query->where('type', $type);
+        }
+        if ($status !== null) {
+            $query->where('status', $status);
+        }  
+        if ($paid_date !== null) {
+            $query->where('paid_date', $paid_date);
+        }
+        if ($is_recurring !== null) {
+            $query->where('is_recurring', $is_recurring);
+        }
+        if ($user_id !== null) {
+            $query->where('user_id', $user_id);
+        }
+        if ($bank_user_id !== null) {
+            $query->where('bank_user_id', $bank_user_id);
+        }
+        if ($total_installments !== null) {
+            $query->where('total_installments', $total_installments);
+        }
+        if ($current_installment !== null) {
+            $query->where('current_installment', $current_installment);
+        }
+        return $query->get();
+    }
 }
