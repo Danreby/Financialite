d5cb1ab77e4e8fcf0452b4b94c6207a2b0067976
Bernardo Rolim <rolimbernardo6@gmail.com>
2026-01-08 14:21:46 -0300

    Testando favicon

--- resources/js/Pages/About.jsx
+++ resources/js/Pages/About.jsx
@@ -159,7 +159,7 @@ export default function About() {
 						<p className="text-base sm:text-lg">
 							Financialite é uma aplicação em constante evolução, desenvolvida com as tecnologias
 							mais modernas para garantir uma experiência de usuário excepcional. Novos recursos são
-							adicionados regularmente, refinando fluxos conforme o uso diário. É possivel analisar o codigo acessando o 
+							adicionados regularmente, refinando fluxos conforme o uso diário. É possivel analisar o codigo acessando o  
 							<a href="https://github.com/Danreby/financialite" target="_blank" rel="noopener noreferrer" className="font-semibold text-blue-500 hover:text-blue-600 dark:hover:text-blue-400 transition-colors duration-200">
 							repositório
 							</a> no GitHub.
